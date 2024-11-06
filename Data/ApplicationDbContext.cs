1e182022c6d6edf584b1c02119b1ec24faa38a4f
Ricardo-we <ricardo0406dev@gmail.com>
2024-11-05 20:27:07 -0600

    Completed first part

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using MockPruebaTecnica.Models;
+
+namespace MockPruebaTecnica.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using MockPruebaTecnica.Data;
+
 namespace MockPruebaTecnica
 {
 	public class Program
@@ -9,10 +12,13 @@ namespace MockPruebaTecnica
 			// Add services to the container.
 			builder.Services.AddControllersWithViews();
 
-			var app = builder.Build();
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
-			// Configure the HTTP request pipeline.
-			if (!app.Environment.IsDevelopment())
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
 			{
 				app.UseExceptionHandler("/Home/Error");
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
